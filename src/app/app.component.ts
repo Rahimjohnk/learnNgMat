2f9b69a0bf930b44d84405419d9bccc32d544ca8
Rahim <k.rahim001@gmail.com>
2023-02-22 19:13:06 +0530

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
+/** @title drawer with custom escape and backdrop click behavior */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  @ViewChild('drawer', { static: true }) public drawer!: MatDrawer;
   title = 'learnNgMat';
+  showFiller = true;
+
+  ngOnInit() {
+    this.drawer.open();
+  }
+
 }
